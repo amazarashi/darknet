211e2ecbc6bbf1b4ad200e305ee9cb7981527d1a
suguru <u.sgr.22114w@gmail.com>
2017-04-22 22:09:10 +0900

    optimizer was customed

--- amaz_optimizer.py
+++ amaz_optimizer.py
@@ -19,7 +19,7 @@ class Optimizers(object):
 
 class OptimizerDarknet(Optimizers):
 
-    def __init__(self,model=None,lr=0.1,momentum=0.9,epoch=160,schedule=(60,90),weight_decay=5.0e-4,decay_power=4,batch=64):
+    def __init__(self,model=None,lr=0.1,momentum=0.9,epoch=160,schedule=(100,175,225),weight_decay=5.0e-4,decay_power=4,batch=64):
         super(OptimizerDarknet,self).__init__(model,epoch)
         self.lr = lr
         self.decay_power = decay_power #polynominal rate decay
@@ -34,6 +34,8 @@ class OptimizerDarknet(Optimizers):
     def update_parameter(self,current_epoch):
         # if current_epoch in self.schedule:
         new_lr = self.lr * (1 - self.batch/self.data_length) ** self.decay_power
+        if current_epoch in self.schedule:
+            new_lr = new_lr * 0.1
         self.lr = new_lr
         self.optimizer.lr = new_lr
         print("optimizer was changed to {0}..".format(new_lr))
