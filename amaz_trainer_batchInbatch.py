a0752bc025f8edb41736ed8f548976c097bf4267
suguru <u.sgr.22114w@gmail.com>
2017-05-02 16:09:10 +0900

    load from external hdd

--- amaz_trainer_batchInbatch.py
+++ amaz_trainer_batchInbatch.py
@@ -16,23 +16,10 @@ import amaz_log
 import amaz_augumentationCustom
 import amaz_imagenet
 import sys
-import subprocess
 
 sampling = amaz_sampling.Sampling()
 
 xp = cuda.cupy
-
-DEFAULT_ATTRIBUTES = (
-    'index',
-    'uuid',
-    'name',
-    'timestamp',
-    'memory.total',
-    'memory.free',
-    'memory.used',
-    'utilization.gpu',
-    'utilization.memory'
-)
 
 class Trainer(object):
 
@@ -88,15 +75,6 @@ class Trainer(object):
         meta = np.array(dd["meta"])
         return (train_key,train_len,test_key,test_len,meta)
 
-    def get_gpu_info(nvidia_smi_path='nvidia-smi', keys=DEFAULT_ATTRIBUTES, no_units=True):
-        nu_opt = '' if not no_units else ',nounits'
-        cmd = '%s --query-gpu=%s --format=csv,noheader%s' % (nvidia_smi_path, ','.join(keys), nu_opt)
-        output = subprocess.check_output(cmd, shell=True)
-        lines = output.split('\n')
-        lines = [ line.strip() for line in lines if line.strip() != '' ]
-
-        return [ { k: v for k, v in zip(keys, line.split(', ')) } for line in lines ]
-
     def train_one(self,epoch):
         model = self.model
         optimizer = self.optimizer
@@ -148,7 +126,6 @@ class Trainer(object):
             print("koko3")
             print("koko3")
             print(str(sys.getsizeof(t)))
-            print(self.get_gpu_info())
             print("#######")
             print("#######")
             print("#######")
