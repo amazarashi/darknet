7eb586028463b1daf29e2252140c0fe95fd740ab
suguru <u.sgr.22114w@gmail.com>
2017-05-02 13:13:48 +0900

    load from external hdd

--- amaz_trainer_batchInbatch.py
+++ amaz_trainer_batchInbatch.py
@@ -45,7 +45,7 @@ class Trainer(object):
     def check_gpu(self, gpu):
         if gpu >= 0:
             cuda.get_device(gpu).use()
-            self.to_gpu()
+            self.model.to_gpu()
             return True
         return False
 
