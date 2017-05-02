14ad9f008c45e40f6c03ea3cd07a59b59a05f04b
suguru <u.sgr.22114w@gmail.com>
2017-05-02 15:29:23 +0900

    load from external hdd

--- amaz_trainer_batchInbatch.py
+++ amaz_trainer_batchInbatch.py
@@ -59,7 +59,7 @@ class Trainer(object):
         else:
             print('loading ' + self.load_model)
             serializers.load_npz(load_model, model)
-        self.check_gpu(gpu)
+        self.check_gpu(self.gpu)
 
     def init_dataset(self):
         d = open("imagenet.pkl","rb")
