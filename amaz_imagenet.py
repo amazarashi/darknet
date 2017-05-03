8db61b016ab75ca185379e0d24da7533fd1a1240
suguru <u.sgr.22114w@gmail.com>
2017-05-03 23:23:58 +0900

    fa

--- amaz_imagenet.py
+++ amaz_imagenet.py
@@ -126,8 +126,8 @@ class ImageNet(object):
             img = cv2.cvtColor(np.array(img),cv2.COLOR_GRAY2RGB)
         transfromedImg = np.asarray(img).transpose(2,0,1).astype(np.float32)/255.
         resimg = amaz_augumentation.Augumentation().Z_score(transfromedImg)[:3]
-        print(imgpath)
-        print(resimg.shape)
+        # print(imgpath)
+        # print(resimg.shape)
         return resimg
 
     def loadImageDataFromKey(self,sampled_key_lists,dataKeyList,train_or_test):
